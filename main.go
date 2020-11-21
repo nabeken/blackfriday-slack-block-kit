f552bce0311861d56b5ac447cb66c46ecb8de77d
TANABE Ken-ichi <nabeken@tknetworks.org>
2020-11-21 18:18:45 +0900

    put the buildinfo

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -11,10 +12,34 @@ import (
 	bf "github.com/russross/blackfriday/v2"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func versionInfo() {
+	fmt.Fprintf(os.Stderr, "Version: %s\nCommit: %s\nBuiltAt: %s\n", version, commit, date)
+}
+
 func main() {
 	debug := flag.Bool("debug", false, "Enable debug output")
+	showVersion := flag.Bool("version", false, "show version info")
+
+	origUsage := flag.Usage
+	flag.Usage = func() {
+		origUsage()
+		fmt.Fprintf(os.Stderr, "\n")
+		fmt.Fprintf(os.Stderr, "== Build Info ==\n")
+		versionInfo()
+	}
 
 	flag.Parse()
+
+	if *showVersion {
+		flag.Usage()
+		os.Exit(0)
+	}
 
 	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
